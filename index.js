1b8894828910f5057b4dab8ba8387383bb36b0d0
Xinyi Yao <xinyi.yao823@gmail.com>
2025-01-12 20:46:47 -0600

    Get request

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const https = require('https');
-const fs = require('fs');
+const http = require('http');
+const fs = require('fs'); //interact with file system
+const url = require('url');
 
 const port = 8080;
-const server = https.createServer();
+const server = http.createServer();
+
+//start server and make sure port is running
+server.listen(port, () => {
+    console.log (`Server running on localhost:${port}`)
+})
+
+
+http.get('http://localhost:8080', res => {
+  if (res.status == 200) {
+    return ('hi')
+  }
+});
+
+// fs.readFile('/home/xinyiyao/top-node-project/index.html','utf8', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
